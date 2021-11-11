8b4a40c6a2593d01b5d282bcc01d13599517bb2c
alex-velikiy <93390622+alex-velikiy@users.noreply.github.com>
2021-11-11 15:38:57 +0300

    Add files via upload

---
+++ appviaa/key.js
@@ -0,0 +1,13 @@
+export default {
+firebaseConfig = {
+  apiKey: "AIzaSyBRMGdUoaCd24jwjzcsqDB8tfcF2Oj6hTA",
+  authDomain: "viaapp-34e1a.firebaseapp.com",
+  databaseURL: "https://viaapp-34e1a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "viaapp-34e1a",
+  storageBucket: "viaapp-34e1a.appspot.com",
+  messagingSenderId: "961955931586",
+  appId: "1:961955931586:web:e06d2048b0f3ba949c625b",
+  measurementId: "G-EMEZDLT7BZ"
+	}
+
+}
